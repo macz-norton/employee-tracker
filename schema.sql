ab11e4e8e5a587c4de0d1e1cd2701ec5043841d4
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-12-22 20:19:11 -0800

    Updated foreign key syntax

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE department (
     id INT AUTO_INCREMENT,
     name VARCHAR(30),
     PRIMARY KEY (id)
- )
+ );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT,
@@ -16,8 +16,8 @@ CREATE TABLE role (
     salary DECIMAL(8,2),
     department_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(department_id)
-)
+    FOREIGN KEY (id) REFERENCES department(id)
+);
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT,
@@ -26,6 +26,5 @@ CREATE TABLE employee (
     role_id INT,
     manager_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES role(role_id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
-)
+    FOREIGN KEY (id) REFERENCES role(id)
+);
