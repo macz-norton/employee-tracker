2806465c34ace1c5e015f097631131414fdf9419
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-12-23 18:02:03 -0800

    Added switch case for different actions

--- db/index.js
+++ db/index.js
@@ -1,1 +1,37 @@
+const inquirer = require("inquirer");
+
 const db = required("./db");
+
+function askForAction() {
+
+    inquirer.prompt({
+        message: "Choose something to do",
+        name: "action",
+        type: "list",
+        choices: [
+            "VIEW_DEPARTMENTS",
+            "VIEW ROLES",
+            "VIEW_EMPLOYEES",
+            "QUIT"
+        ]
+    }).then(() => {
+
+        switch(res.action) {
+
+            case "VIEW_DEPARTMENTS":
+                return;
+
+            case "VIEW_ROLES":
+                return;
+
+            case "VIEW_EMPLOYEES":
+                return;
+            
+            default:
+                connection.end();
+
+        }
+    })
+
+}
+
