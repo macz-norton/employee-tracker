2805da5a9c07323091493367f9d71b0e73627e7d
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-12-23 17:59:36 -0800

    Updated index.js files with new functions with SQL queries

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const connection = require("./connection");
 
 module.exports = {
     getDepartments() {
-        return connection
+        return connection.query("SELECT * FROM departments");
+    },
+    getRoles() {
+        return connection.query("SELECT * FROM roles");
+    },
+    getEmployees() {
+        return connection.query("SELECT * FROM employees")
     }
 }
